c970d0a5d51eb8685aac95fe3ee715f577418276
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-02-06 19:42:54 +0100

    Layout completed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,141 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="my-8">
+    <div class="mb-12">
+      <img src="../assets/slide1.jpg" alt="mandalorian" class="rounded" />
+    </div>
+    <div class="categories flex items-center mb-12 -mx-4">
+      <div class="w-1/5 px-4">
+        <a
+          href=""
+          class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-custom p-4 hover:bg-gray-900"
+        >
+          <img src="../assets/disney_logo.svg" alt="disney" />
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a
+          href=""
+          class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-custom p-4 hover:bg-gray-900"
+        >
+          <img src="../assets/pixar_logo.svg" alt="disney" />
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a
+          href=""
+          class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-custom p-4 hover:bg-gray-900"
+        >
+          <img src="../assets/marvel_logo.svg" alt="disney" />
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a
+          href=""
+          class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-custom p-4 hover:bg-gray-900"
+        >
+          <img src="../assets/star_wars_logo.svg" alt="disney" />
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a
+          href=""
+          class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-custom p-4 hover:bg-gray-900"
+        >
+          <img src="../assets/national_geographic_logo.svg" alt="disney" />
+        </a>
+      </div>
+    </div>
+    <!-- end of categories -->
+    <div class="recommended px-6 mb-12">
+      <h2 class="text-gray-500 mb-1">Recommended For You</h2>
+      <div class="flex item-center">
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/star_wars_a_new_hope.jpg" alt="star wars"
+          /></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""><img src="../assets/snow_white.jpg" alt="star wars"/></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/black_panther.jpg" alt="star wars"
+          /></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""><img src="../assets/zootopia.jpg" alt="star wars"/></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/one_strange_rock.jpg" alt="star wars"
+          /></a>
+        </div>
+      </div>
+    </div>
+    <!--continue watching  -->
+    <div class="recommended px-6 mb-12">
+      <h2 class="text-gray-500 mb-1">Continue Watching</h2>
+      <div class="flex item-center">
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/the_sound_of_music.jpg" alt="star wars"
+          /></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/high_school_musical.jpg" alt="star wars"
+          /></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""><img src="../assets/cinderella.jpg" alt="star wars"/></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/remember_the_titans.jpg" alt="star wars"
+          /></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/avengers_infinity_war.jpg" alt="star wars"
+          /></a>
+        </div>
+      </div>
+    </div>
+    <!-- end -->
+    <!-- disney+ originals -->
+    <div class="recommended px-6 mb-12">
+      <h2 class="text-gray-500 mb-1">Disney+ Originals</h2>
+      <div class="flex item-center">
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/lady_and_the_tramp.jpg" alt="star wars"
+          /></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/the_mandalorian.jpg" alt="star wars"
+          /></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""><img src="../assets/cinderella.jpg" alt="star wars"/></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/forky_asks_a_question.jpg" alt="star wars"
+          /></a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href=""
+            ><img src="../assets/marvel_hero_project.jpg" alt="star wars"
+          /></a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
